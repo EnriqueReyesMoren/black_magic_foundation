9f19aa78ff5f2d464a983086b424511f79503894
anuarhdz <anuar@hey.com>
2022-04-23 04:46:04 -0500

    static boilerplate integration almost complete, sidebar and content 100% integrated with cms

--- config/general.php
+++ config/general.php
@@ -14,39 +14,53 @@ $isDev = App::env('ENVIRONMENT') === 'dev';
 $isProd = App::env('ENVIRONMENT') === 'production';
 
 return [
-    // Default Week Start Day (0 = Sunday, 1 = Monday...)
-    'defaultWeekStartDay' => 1,
-
-    // Whether generated URLs should omit "index.php"
-    'omitScriptNameInUrls' => true,
-
-    // The URI segment that tells Craft to load the control panel
-    'cpTrigger' => App::env('CP_TRIGGER') ?: 'admin',
-
-    // The secure key Craft will use for hashing and encrypting data
-    'securityKey' => App::env('SECURITY_KEY'),
-
-    // Whether Dev Mode should be enabled (see https://craftcms.com/guides/what-dev-mode-does)
-    'devMode' => $isDev,
-
-    // Whether administrative changes should be allowed
-    'allowAdminChanges' => $isDev,
-
-    // Whether crawlers should be allowed to index pages and following links
-    'disallowRobots' => !$isProd,
-
-    'aliases' => [
-        '@web' => App::env('PRIMARY_SITE_URL'),
-        '@siteUrl' => App::env('PRIMARY_SITE_URL'),
-        '@mediaBaseUrl' => App::env('MEDIA_BASE_URL'),
-        '@mediaBasePath' => App::env('MEDIA_BASE_PATH')
+    '*' => [
+        // Default Week Start Day (0 = Sunday, 1 = Monday...)
+        'defaultWeekStartDay' => 1,
+    
+        // Whether generated URLs should omit "index.php"
+        'omitScriptNameInUrls' => true,
+    
+        // The URI segment that tells Craft to load the control panel
+        'cpTrigger' => App::env('CP_TRIGGER') ?: 'admin',
+    
+        // The secure key Craft will use for hashing and encrypting data
+        'securityKey' => App::env('SECURITY_KEY'),
+    
+        // Whether Dev Mode should be enabled (see https://craftcms.com/guides/what-dev-mode-does)
+        'devMode' => $isDev,
+    
+        // Whether administrative changes should be allowed
+        'allowAdminChanges' => $isDev,
+    
+        // Whether crawlers should be allowed to index pages and following links
+        'disallowRobots' => !$isProd,
+    
+        'aliases' => [
+            '@web' => App::env('PRIMARY_SITE_URL'),
+            '@siteUrl' => App::env('PRIMARY_SITE_URL'),
+            '@mediaBaseUrl' => App::env('MEDIA_BASE_URL'),
+            '@mediaBasePath' => App::env('MEDIA_BASE_PATH')
+        ],
+    
+        'transformGifs' => false,
+    
+        'transformSvgs' => true,
+    
+        'upscaleImages' => false,
+    
+        'defaultSearchTermOptions' => [
+            'subLeft' => true,
+        ],
     ],
 
     'dev' => [
+        'devMode' => true,
+
         'aliases' => [
             '@webroot' => dirname(__DIR__) . 'public/',
             'backupCommand' => App::env('BACKUP_COMMAND'),
             'restoreCommand' => App::env('RESTORE_COMMAND'),
-        ]
-    ]
+        ],
+    ],
 ];
